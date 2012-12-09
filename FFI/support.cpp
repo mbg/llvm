73820bc3b4ab9d04529a5abffe826f10291b543d
Michael Gale <michael.gale@syngate.co.uk>
2012-12-09 04:11:25 +0000

    Initial commit

---
+++ FFI/support.cpp
@@ -0,0 +1,40 @@
+#define __STDC_LIMIT_MACROS
+#define __STDC_CONSTANT_MACROS
+
+#include "llvm/LLVMContext.h"
+#include "llvm/Module.h"
+#include "llvm/PassManager.h"
+#include "llvm/Pass.h"
+#include "llvm/ADT/Triple.h"
+#include "llvm/Support/IRReader.h"
+#include "llvm/CodeGen/LinkAllAsmWriterComponents.h"
+#include "llvm/CodeGen/LinkAllCodegenComponents.h"
+#include "llvm/MC/SubtargetFeature.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/FormattedStream.h"
+#include "llvm/Support/ManagedStatic.h"
+#include "llvm/Support/PluginLoader.h"
+#include "llvm/Support/PrettyStackTrace.h"
+#include "llvm/Support/ToolOutputFile.h"
+#include "llvm/Support/Host.h"
+#include "llvm/Support/Signals.h"
+#include "llvm/Support/TargetRegistry.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Target/TargetData.h"
+#include "llvm/Target/TargetMachine.h"
+
+#include "llvm-c/Core.h"
+
+using namespace llvm;
+
+void LLVMGenerateObjectFile(LLVMModuleRef modulePtr, char* filename)
+{
+	Module &module = *unwrap(modulePtr);
+	Triple triple(module.getTargetTriple());
+
+	if (triple.getTriple().empty())
+		triple.setTriple(sys::getDefaultTargetTriple());
+
+
+}
